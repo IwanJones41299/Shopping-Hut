28ac98c253eab3be44a1b571e26d62e985679631
IwanJones41299 <iwanjones41299@gmail.com>
2020-10-11 19:23:39 +0100

    Dashboard design started

--- app/client/src/Components/dashboard.js
+++ app/client/src/Components/dashboard.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Dashboard = (props) => {
-    return(
-        <h1>
-            Dashboard
-        </h1>
-    )
-}
+  return (
+    <div className="container-fluid dashboard-container">
+      <h1 className="welcome_banner">Welcome (username)</h1>
+      <nav className="navbar navbar-dark bg-light bottomNav">
+        <div className="justify-content-center">
+          <ul className="navbar-nav mr-auto">
+          <li className="nav-item nav-link">Contact</li>
+          <li className="nav-item nav-link">Contact</li>
+          <li className="nav-item nav-link">Contact</li>
+          <li className="nav-item nav-link">Contact</li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default Dashboard;
