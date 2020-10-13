98b6687043fabb2dc6b853d78fb7b1f8002324a4
IwanJones41299 <iwanjones41299@gmail.com>
2020-10-13 16:46:43 +0100

    update

---
+++ app/client/src/Components/login.js
@@ -0,0 +1,105 @@
+import React, { useState, useContext } from "react";
+import AuthService from "../Services/AuthService";
+import Message from "../Components/message";
+import { AuthContext } from "../Context/AuthContext";
+import uname_img from "../images/login.svg";
+import password_img from "../images/password.svg";
+
+const Login = (props) => {
+
+  const [user, setUser] = useState({ username: "", password: "" });
+  const [message, setMessage] = useState(null);
+  const authContext = useContext(AuthContext);
+
+  const onChange = (e) => {
+    e.preventDefault();
+    setUser({ ...user, [e.target.name]: e.target.value });
+    //Delte following line, once testing is complete!
+    console.log(user);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    AuthService.login(user).then((data) => {
+      console.log(data);
+      const { isAuthenticated, user, message } = data;
+      if (isAuthenticated) {
+        authContext.setUser(user);
+        authContext.setIsAuthenticated(isAuthenticated);
+        props.history.push("/dashboard");
+      } else setMessage(message);
+    });
+  };
+
+  return (
+    <div className="login-container">
+      <div className="container-fluid">
+        <div className="card-body">
+            <h3 className="signin">Sign In</h3>
+          <form onSubmit={onSubmit}>
+            <div className="input-group form-group">
+              <div className="input-group-prepend">
+                <span className="input-group-text">
+                  <img src={uname_img} className="login_img"></img>
+                </span>
+              </div>
+              <input
+                type="text"
+                className="form-control"
+                name="username"
+                onChange={onChange}
+                placeholder="username..."
+              ></input>
+            </div>
+
+            <div className="input-group form-group">
+              <div className="input-group-prepend">
+                <span className="input-group-text">
+                  <img src={password_img} className="login_img"></img>
+                </span>
+              </div>
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                onChange={onChange}
+                placeholder="password..."
+              ></input>
+            </div>
+
+            <button className="btn btn-block login2" type="submit">
+              Login
+            </button>
+            {/* Google login button */}
+            <button className="btn btn-block google-login" type="submit">
+              Google
+            </button>
+            {/* Facebook login button */}
+            <button className="btn btn-block facebook-login" type="submit">
+              Facebook
+            </button>
+            <div className="card-body">
+              <div className="pwd-reset">
+                Forgotton your
+                <a href="/" className="pwd-reset">
+                  {" "}
+                  Password
+                </a>
+              </div>
+              <div className="reg-link">
+                Not signed up yet?
+                <a href="/register" className="reg-link">
+                  {" "}
+                  Register now
+                </a>
+              </div>
+            </div>
+          </form>
+          {message ? <Message message={message} /> : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ app/client/src/Components/navbar.js
@@ -0,0 +1,59 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import AuthService from "../Services/AuthService";
+import { AuthContext } from "../Context/AuthContext";
+
+const Navbar = (props) => {
+  const { isAuthenticated, setIsAuthenticated, setUser } = useContext(
+    AuthContext
+  );
+
+  const onClickLogoutHandler = () => {
+    AuthService.logout().then((data) => {
+      if (data.sucess) {
+        setUser(data.user);
+        setIsAuthenticated(false);
+      }
+    });
+  };
+
+  const unauthenticatedNavbar = () => {
+    return (
+      <>
+        <Link to="/contact">
+          <li className="nav-item nav-link">Contact</li>
+        </Link>
+      </>
+    );
+  };
+
+  const authenticatedNavbar = () => {
+    return (
+      <>
+        <Link to="/logout">
+          <button className="btn btn-warning" onClick={onClickLogoutHandler}>
+            Logout
+          </button>
+        </Link>
+      </>
+    );
+  };
+
+  return (
+    <nav className="navbar navbar-dark bg-light app-navbar">
+      <Link to="/">
+        <div className="navbar-brand">
+            <h5 className="nav_Title">Shopping Hut</h5>
+        </div>
+      </Link>
+        <div className="justify-content-end">
+            <ul className="navbar-nav mr-auto">
+            {!isAuthenticated ? unauthenticatedNavbar() : authenticatedNavbar()}
+          </ul>
+        </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+//{!isAuthenticated ? unauthenticatedNavbar() : authenticatedNavbar()}
