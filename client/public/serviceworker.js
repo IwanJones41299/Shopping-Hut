4b5c2a3a47508fcca016d97e68e345b2b1c8ba06
IwanJones41299 <iwanjones41299@gmail.com>
2021-01-29 19:11:18 +0000

    pwa setup - uncomplete

--- client/public/serviceworker.js
+++ client/public/serviceworker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "version-1";
-const urlsToCache = [ 'index.html' ]; //Add the files I want to have the app use with the online functionality.
+const urlsToCache = [ 'index.html', 'offline.html' ]; //Add the files I want to have the app use with the online functionality.
 
 const self = this;
 
@@ -16,10 +16,28 @@ self.addEventListener('install', (event) => {
 
 //Listen for request
 self.addEventListener('fetch', (event) => {
-    
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request)
+                    .catch(() => caches.match('offline.html'))
+            })
+    )
 });
 
 //Activate
 self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
 
+    event.waitUntil(
+        caches.keys()
+            .then((cacheNames) => Promise.all(
+                cacheNames.map((cacheName) => {
+                    if(!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            ))
+    )
 });
