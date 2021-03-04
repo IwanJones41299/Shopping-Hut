4d2f41a3dee0b975112eacfb36bf8d8ac3575122
IwanJones41299 <iwanjones41299@gmail.com>
2021-03-04 19:10:33 +0000

    fixing installability

--- client/public/serviceworker.js
+++ client/public/serviceworker.js
@@ -1,45 +1,41 @@
-const cacheName = "static-service_worker";
-const assets = [
-    'index.html',
-    'offline.html',
-    'images/logo.png'
-]
+const CACHE_NAME = "version-1";
+const urlsToCache = [ 'index.html', 'offline.html', 'logo.png' ];
 
 const self = this;
 
-//Install service worker
-self.addEventListener('install', (evt) => {
-    evt.waitUntil(
-        caches.open(cacheName)
-        .then((cache) => {
-            console.log('Opening cache');
+//Install SW
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened Cache')
 
-            return cache.addAll(assets);
-        })
-    )
+                return cache.addAll(urlsToCache)
+            })
+        )
 });
 
 //Listen for requests
-self.addEventListener('fetch', (evt) => {
-    evt.respondWith(
-        caches.match(evt.request)
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
         .then(() => {
-            return fetch(evt.request)
-            .catch(() => caches.match('offline.html'))
+            return fetch(event.request)
+                .catch(() => caches.match('offline.html'))
         })
     )
 });
 
-//Activate service worker
-self.addEventListener('activate', (evt) => {
-    const cacheWhiteList = [];
-    cacheWhiteList.push(cacheName);
-
-    evt.waitUntil(
-        caches.keys().then((cacheName) => Promise.all(
-            cacheName.map((cacheName) => {
-                if(!cacheWhiteList.includes(cacheName)) {
-                    return caches.delete(cacheName);
+//Activate the SW
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME)
+    
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheNames) => {
+                if(!cacheWhitelist.includes(cacheNames)){
+                    return caches.delete(cacheNames);
                 }
             })
         ))
